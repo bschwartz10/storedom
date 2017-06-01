f1a4bdfeae2db619c9836e5cc8ec24177ccc50c0
bschwartz10 <bschwartz92@gmail.com>
2017-06-01 10:09:10 -0600

    api coming through in controller, working on getting data to show up in view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,9 @@
 class SearchController < ApplicationController
+
+  def index
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=longName,city,distance,phone,storeType,&pageSize=10&apiKey=7a3heu7emrjz6qbsugmepbv6")
+    @raw_stores = JSON.parse(response.body, :symbolize_names => true)
+    @stores = JSON.parse(response.body, :symbolize_names => true)[:stores]
+  end
+
 end
--- spec/features/user_can_search_for_stores_based_on_zipcode_spec.rb
+++ spec/features/user_can_search_for_stores_based_on_zipcode_spec.rb
@@ -2,24 +2,25 @@ require 'rails_helper'
 
 RSpec.feature "user visits root path and types in zipcode" do
   scenario "redirected to stores index page and is shown the 10 closet stores"do
+    VCR.use_cassette("bb_search_index_page") do
+      visit '/'
+      fill_in :search, with: "80202"
+      click_on "Search"
 
-    visit '/'
-    fill_in :search, with: "80202"
-    click_on "Search"
-
-    expect(current_path).to eq("/search")
-    expect(page).to have_content("Stores within 25 miles of 80202")
-    expect(page).to have_content("16 Total Stores")
-    # expect to see 10 results
-    expect(page).to have_content("Name: BEST BUY MOBILE - CHERRY CREEK SHOPPING CENTER")
-    expect(page).to have_content("Name: BEST BUY - SOUTHGLENN")
-    expect(page).to have_content("City: DENVER")
-    expect(page).to have_content("City: CENTENNIAL")
-    expect(page).to have_content("Distance: 3.45 Miles")
-    expect(page).to have_content("Distance: 11.02 Miles")
-    expect(page).to have_content("Phone: 303-270-9189")
-    expect(page).to have_content("Phone: 303-797-3246")
-    expect(page).to have_content("StoreType: Mobile SAS")
-    expect(page).to have_content("StoreType: Big Box")
+      expect(current_path).to eq("/search")
+      expect(page).to have_content("Stores within 25 miles of 80202")
+      expect(page).to have_content("16 Total Stores")
+      # expect to see 10 results
+      expect(page).to have_content("Name: BEST BUY MOBILE - CHERRY CREEK SHOPPING CENTER")
+      expect(page).to have_content("Name: BEST BUY - SOUTHGLENN")
+      expect(page).to have_content("City: DENVER")
+      expect(page).to have_content("City: CENTENNIAL")
+      expect(page).to have_content("Distance: 3.45 Miles")
+      expect(page).to have_content("Distance: 11.02 Miles")
+      expect(page).to have_content("Phone: 303-270-9189")
+      expect(page).to have_content("Phone: 303-797-3246")
+      expect(page).to have_content("StoreType: Mobile SAS")
+      expect(page).to have_content("StoreType: Big Box")
+    end
   end
 end
