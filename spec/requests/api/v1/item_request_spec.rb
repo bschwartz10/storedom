c51be7f165c61c37367654c8fbd94d712d725378
bschwartz10 <bschwartz92@gmail.com>
2017-06-01 11:24:29 -0600

    index and show working or internal api

--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -21,5 +21,46 @@ RSpec.describe "Item Api" do
       get '/api/v1/items'
 
       expect(response).to be_success
+      items = JSON.parse(response.body)
+      expect(items.count).to eq(3)
   end
+
+  it "show page returns one item" do
+    id = Item.create!(
+          name: Faker::Commerce.product_name,
+          description: Faker::Lorem.paragraph,
+          image_url: "http://robohash.org/1.png?set=set2&bgset=bg1&size=200x200"
+          ).id
+
+    get "/api/v1/items/#{id}"
+
+    expect(response).to be_success
+    item = JSON.parse(response.body)
+    expect(item["id"]).to eq(id)
+  end
+
+  it "can create an item" do
+    item_params = {
+                    name: "Brett",
+                    description: Faker::Lorem.paragraph,
+                    image_url: "http://robohash.org/1.png?set=set2&bgset=bg1&size=200x200"
+                  }
+
+    post '/api/v1/items', params: item_params
+
+    item = Item.last
+    expect(response).to be_success
+    expect(item.name).to eq("Brett")
+  end
+  # it "can update an item" do
+  #   item = Item.create!(
+  #         name: Faker::Commerce.product_name,
+  #         description: Faker::Lorem.paragraph,
+  #         image_url: "http://robohash.org/1.png?set=set2&bgset=bg1&size=200x200"
+  #         )
+  #
+  #   previous_name = item.name
+  #
+  #
+  # end
 end
