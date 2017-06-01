d12df2bc4df8d9e248d9b77972173746de2683c4
bschwartz10 <bschwartz92@gmail.com>
2017-06-01 09:53:05 -0600

    write feature test, now implementing it

--- spec/features/user_can_search_for_stores_based_on_zipcode_spec.rb
+++ spec/features/user_can_search_for_stores_based_on_zipcode_spec.rb
@@ -9,6 +9,17 @@ RSpec.feature "user visits root path and types in zipcode" do
 
     expect(current_path).to eq("/search")
     expect(page).to have_content("Stores within 25 miles of 80202")
-    expect(page).to have_content("Stores within 25 miles of 80202")
+    expect(page).to have_content("16 Total Stores")
+    # expect to see 10 results
+    expect(page).to have_content("Name: BEST BUY MOBILE - CHERRY CREEK SHOPPING CENTER")
+    expect(page).to have_content("Name: BEST BUY - SOUTHGLENN")
+    expect(page).to have_content("City: DENVER")
+    expect(page).to have_content("City: CENTENNIAL")
+    expect(page).to have_content("Distance: 3.45 Miles")
+    expect(page).to have_content("Distance: 11.02 Miles")
+    expect(page).to have_content("Phone: 303-270-9189")
+    expect(page).to have_content("Phone: 303-797-3246")
+    expect(page).to have_content("StoreType: Mobile SAS")
+    expect(page).to have_content("StoreType: Big Box")
   end
 end
