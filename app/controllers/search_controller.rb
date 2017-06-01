de25beb56ac0e3a393e99115613ea7533933027e
bschwartz10 <bschwartz92@gmail.com>
2017-06-01 12:10:25 -0600

    try to refactor service but couldn't get it to pass tests

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,9 +4,6 @@ class SearchController < ApplicationController
     @zipcode = params[:search]
     @stores = BestBuyStore.find_by_zip_code(@zipcode)
     @total = BestBuyService.zip_code_by(@zipcode)[:total]
-    # response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=longName,city,distance,phone,storeType,&pageSize=10&apiKey=7a3heu7emrjz6qbsugmepbv6")
-    # @raw_stores = JSON.parse(response.body, :symbolize_names => true)
-    # @stores = JSON.parse(response.body, :symbolize_names => true)[:stores]
   end
 
 end
